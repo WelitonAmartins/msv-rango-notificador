0b04f1d154213ff0df4dcdffbbe02f7817d819cf
Weliton Martins <weliton.martins@yaman.com>
2020-12-08 21:13:31 -0300

    commit inicial

---
+++ src/main/java/br/com/unilito/msvrangonotificador/MsvRangoNotificadorApplication.java
@@ -0,0 +1,13 @@
+package br.com.unilito.msvrangonotificador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvRangoNotificadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvRangoNotificadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/unilito/msvrangonotificador/MsvRangoNotificadorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unilito.msvrangonotificador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvRangoNotificadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
